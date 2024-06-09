bfa5e54d290669e11d1a5b414e463d2072c24fc5
Roy Yosef <royosef10@gmail.com>
2024-06-09 16:37:14 +0300

    feat(uk-us-words): uk us dictionary enricher

--- Crawler/BBCSpiders.py
+++ Crawler/BBCSpiders.py
@@ -1,25 +1,18 @@
-import scrapy
-import os
-import re
-import csv
+import os, json
 import datetime
-import operator
-from string import printable
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
 from scrapy.signalmanager import SignalManager
 from scrapy import signals
-from scrapy.xlib.pydispatch import dispatcher
-from BBCSpider.items import WebSite
+from pydispatch import dispatcher
+from items import WebSite
 
 class BBCSpiders(CrawlSpider):
 	name = "BBCSpiders"
 	allowed_domains = ["bbc.com"]
 	scienceCategories = []
 	
-	visitedUrlsGeneral = []
-	visitedUrlsScience = []
+	visitedUrls = {}
 	savedToday = False
 
 	
@@ -66,82 +59,94 @@ class BBCSpiders(CrawlSpider):
 			 				follow=True),
 		
 		Rule(LinkExtractor(allow=(),
-						    deny=(r'.*(m\.|\.test\.|\.stage\.|%|comments|\/live\/|\/athlete\/|\/weather\/).*'),
-						  	restrict_xpaths="//div[@role='main']"),
+						    deny=(r'.*(m\.|\.test\.|\.stage\.|%|comments|\/live\/|\/athlete\/|\/weather\/).*')),
 			 				follow=True),
     )
 	
 	def parseNews(self, response):
-		science = False
-		for url in self.scienceCategories:
-			if url[:-1] in response.url:
-				science = True
-				break
 		item = WebSite()
 		item['url'] = response.url		
-		item['year'] = datetime.datetime.fromtimestamp(int(response.xpath("//@data-seconds")[0].extract())).year
-			
-		if item['year'] >= 2012 and item['year'] <= 2015:
-			if science is True:
-				self.visitedUrlsScience.append(response.url)
-			else:
-				self.visitedUrlsGeneral.append(response.url)
-			self.logProgress()
-			return item
+		
+		tmp_year = response.xpath("//@data-seconds")
+		
+		if tmp_year is not None and len(tmp_year) > 0:
+			item['year'] = datetime.datetime.fromtimestamp(int(tmp_year[0].extract())).year
+
+		self.logItem(response, item)
+
 	
 	def parseStory(self, response):
-		science = False
-		for url in self.scienceCategories:
-			if url[:-1] in response.url:
-				science = True
-				break
 		item = WebSite()
 		item['url'] = response.url	
-		item['year'] = int(response.xpath("//span[@class='publication-date index-body']//text()")[0].extract()[-4:])
 
-		if item['year'] >= 2012 and item['year'] <= 2015:
-			if science is True:
-				self.visitedUrlsScience.append(response.url)
-			else:
-				self.visitedUrlsGeneral.append(response.url)	
-			self.logProgress()
-			return item
+		tmp_year = response.xpath("//span[@class='publication-date index-body']//text()")
+		
+		if tmp_year is not None and len(tmp_year) > 0:
+			item['year'] = int(tmp_year[0].extract()[-4:])
+
+		self.logItem(response, item)
 	
 	def parseSport(self, response):
-		
 		item = WebSite()
 		item['url'] = response.url
-		item['year'] = datetime.datetime.fromtimestamp(int(response.xpath("//@data-timestamp")[0].extract())).year
 
-		if item['year'] >= 2012 and item['year'] <= 2015:
-			self.visitedUrlsGeneral.append(response.url)
-			self.logProgress()
-			return item
+		tmp_year = response.xpath("//@data-timestamp")
+		
+		if tmp_year is not None and len(tmp_year) > 0:
+			item['year'] = datetime.datetime.fromtimestamp(int(tmp_year[0].extract())).year
+
+		self.logItem(response, item)
+
+		self.logItem(response, item)
+
+		
+	def logItem(self, response, item):
+		if item.get('year') is None:
+			json_ld_script = response.xpath('//script[@type="application/ld+json"]/text()').get()
+
+			if json_ld_script:
+				json_data = json.loads(json_ld_script)
+				date_modified = json_data.get('dateModified', json_data.get('uploadDate'))
+
+				if date_modified is None:
+					return 
+				# print('datetimeVVVVVV')
+				# print(date_modified[:-1])
+				# print(datetime.datetime.fromisoformat(date_modified[:-1]))
+				# print('datetime^^^^^^')
+				item['year'] = datetime.datetime.fromisoformat(date_modified[:-1]).year
+
+		if item.get('year') is not None:
+			if item['year'] <= 24:
+				item['year'] += 2000
+				
+			if item['year'] >= 2012 and item['year'] <= 2023:
+				if item['year'] not in self.visitedUrls:
+					self.visitedUrls[item['year']] = []
+
+				self.visitedUrls[item['year']].append(item['url'])
+				self.logProgress()
+				return item
 
 	def logProgress(self):
 		now = datetime.datetime.now()
 		if now.replace(hour=21, minute=0, second=0, microsecond=0) <= now:
 			if self.savedToday is False:
-				with open("visitedUrlsGeneral"+datetime.datetime.strftime(now, '%Y-%m-%d %H:%M:%S')+".txt", 'a') as f:
-					for url in self.visitedUrlsGeneral:
-						f.write(url+"\n")
-					f.close()
-				with open("visitedUrlsScience"+datetime.datetime.strftime(now, '%Y-%m-%d %H:%M:%S')+".txt", 'a') as f:
-					for url in self.visitedUrlsScience:
-						f.write(url+"\n")
-					f.close()
+
+				for year in self.visitedUrls:
+					with open(f"visitedUrls_{year}_{datetime.datetime.strftime(now, '%Y-%m-%d %H:%M:%S')}.txt", 'a') as f:
+						for url in self.visitedUrls[year]:
+							f.write(url+"\n")
+						f.close()
+
 				self.savedToday = True
 		else:
 			self.savedToday = False
 
 	
 	def spiderClosed(self, spider):		
-		with open("visitedUrlsGeneral.txt", 'a') as f:
-			for url in self.visitedUrlsGeneral:
-				f.write(url+"\n")
-			f.close()
-
-		with open("visitedUrlsScience.txt", 'a') as f:
-			for url in self.visitedUrlsScience:
-				f.write(url+"\n")
-			f.close()
+		for year in self.visitedUrls:
+			with open(f"visitedUrls_{year}.txt", 'a') as f:
+				for url in self.visitedUrls[year]:
+					f.write(url+"\n")
+				f.close()
---
+++ Website/AddUKUSWords/Program.cs
@@ -0,0 +1,145 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+namespace AddUKUSWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = loadUSUKLines("Words-Worldwide-Word-list-UK-US-2009.docx");
+            var instancesMatrixGeneral16 = loadInstancesMatrix("Data2016.csv");
+            var instancesMatrixGeneral17 = loadInstancesMatrix("Data2017.csv");
+            //var instancesMatrixGeneral18p1 = loadInstancesMatrix("Data2018p1.csv");
+            var instancesMatrixGeneral18p2 = loadInstancesMatrix("Data2018p2.csv");
+            var instancesMatrixGeneral19 = loadInstancesMatrix("Data2019.csv");
+
+            var instanceMatrix = mergeMatrices(instancesMatrixGeneral18p2,
+                                                        mergeMatrices(instancesMatrixGeneral19,
+                                                            mergeMatrices(instancesMatrixGeneral16, instancesMatrixGeneral17)));
+            UpdateInstancesMatrix(lines, instanceMatrix);
+            writeCSV(instanceMatrix, "DataUKUS2016-2019.csv");
+        }
+
+        public static Dictionary<string, int> mergeMatrices(Dictionary<string, int> i_Matrix1, Dictionary<string, int> i_Matrix2)
+        {
+            Dictionary<string, int> mergedMatrix = new Dictionary<string, int>();
+            foreach (KeyValuePair<string, int> pair in i_Matrix2)
+            {
+                if (mergedMatrix.ContainsKey(pair.Key.ToLower()))
+                {
+                    mergedMatrix[pair.Key.ToLower()] += pair.Value;
+                }
+                else
+                {
+                    mergedMatrix.Add(pair.Key.ToLower(), pair.Value);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> pair in i_Matrix1)
+            {
+                if (mergedMatrix.ContainsKey(pair.Key.ToLower()))
+                {
+                    mergedMatrix[pair.Key.ToLower()] += pair.Value;
+                }
+                else
+                {
+                    mergedMatrix.Add(pair.Key.ToLower(), pair.Value);
+                }
+            }
+
+            return mergedMatrix;
+        }
+
+        public static void writeCSV(Dictionary<string, int> i_Dict, string i_Path)
+        {
+            StreamWriter sw = File.CreateText(i_Path);
+
+            foreach (KeyValuePair<string, int> pair in i_Dict.OrderByDescending(pair => pair.Value))
+            {
+                sw.Write(pair.Key + "," + pair.Value + "\r\n");
+            }
+
+            sw.Close();
+        }
+
+        public static void UpdateInstancesMatrix(List<string> i_Lines, Dictionary<string, int> i_InstancesMatrix)
+        {
+            List<string> notContained = new List<string>();
+            string[] words;
+            int max;
+
+            foreach (string line in i_Lines)
+            {
+                words = line.Split('\t').Distinct().ToArray();
+                max = -1;
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (i_InstancesMatrix.ContainsKey(words[i]))
+                    {
+                        if (max < i_InstancesMatrix[words[i]])
+                        {
+                            max = i_InstancesMatrix[words[i]];
+                        }
+                        else
+                        {
+                            notContained.Add(words[i]);
+                        }
+                    }
+                    else if (words[i] != "")
+                    {
+                        notContained.Add(words[i]);
+                    }
+                }
+
+                for (int i = 0; max != -1 && i < notContained.Count; i++)
+                {
+                    i_InstancesMatrix[notContained[i]] = max;
+                }
+
+                notContained.Clear();
+            }
+        }
+
+        public static List<string> loadUSUKLines(string i_Path)
+        {
+            List<string> lines = new List<string>();
+
+            using (FileStream stream = new FileStream(i_Path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                DocX document = DocX.Load(stream);
+
+                foreach (Paragraph item in document.Paragraphs)
+                {
+                    lines.Add(item.Text);
+                }
+            }
+
+            return lines;
+        }
+
+        public static Dictionary<string, int> loadInstancesMatrix(string i_Path)
+        {
+            Dictionary<string, int> wordList = new Dictionary<string, int>();
+
+            TextReader data = new StreamReader(new FileStream(i_Path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+            var csv = new CsvReader(data, false);
+
+            wordList.Clear();
+
+            while (csv.ReadNextRecord())
+            {
+                wordList.Add(csv[0], int.Parse(csv[1]));
+            }
+
+            data.Close();
+
+            return wordList;
+        }
+    }
+}
